f098707be6e7a807a01f4fe416d2069cdcdb7d97
wasilmilena <wasil.milena@o2.pl>
2025-01-24 17:38:34 +0100

    zadania.ipynb.pl

--- zadania.ipynb.pl
+++ zadania.ipynb.pl
@@ -30,3 +30,47 @@ kuzyn(X, Y) :- rodzic(A, X), rodzic(B, Y), brat(A, B).
 ?- przodek(jan, anna). %true
 ?- brat(piotr, anna). %true
 ?- kuzyn(X, piotr). %X = dawid
+
+
+%Fakty
+lotnisko(warszawa).
+lotnisko(krakow).
+lotnisko(gdansk).
+lotnisko(poznan).
+polaczenie(warszawa, krakow, 300).
+polaczenie(krakow, gdansk, 200).
+polaczenie(warszawa, gdansk, 500).
+polaczenie(warszawa, poznan, 150).
+polaczenie(poznan, gdansk, 350).
+polaczenie(krakow, warszawa, 300).
+polaczenie(gdansk, krakow, 200).
+polaczenie(gdansk, warszawa, 500).
+polaczenie(gdansk, poznan, 350).
+polaczenie(poznan, warszawa, 150).
+
+%Reguły
+polaczenie(X, Y) :- polaczenie(X, Y, _).
+koszt_podrozy(X, Y, C) :- polaczenie(X, Y, C).
+droga(X, Y, Trasa, C) :- droga(X, Y, [X], Trasa, C).
+droga(X, Y, TrasaSoFar, Trasa, C) :- polaczenie(X, Y, C), reverse([Y|TrasaSoFar], Trasa).
+droga(X, Y, TrasaSoFar, Trasa, C) :-
+    polaczenie(X, Z, C1),
+    Z \= Y,
+    \+ member(Z, TrasaSoFar),
+    droga(Z, Y, [Z|TrasaSoFar], Trasa, C2),
+    C is C1 + C2.
+najkrotsza_droga(X, Y) :-
+    setof((C, Trasa), droga(X, Y, Trasa, C), [(Koszt, NajkrotszaTrasa)|_]),
+    write('Najkrótsza droga z '), write(X), write(' do '), write(Y), nl,
+    write('Trasa: '), write(NajkrotszaTrasa), nl,
+    write('Koszt: '), write(Koszt), write(' PLN'), nl.
+
+?- polaczenie(warszawa, krakow). %true
+?- koszt_podrozy(warszawa, gdansk, C). %C = 500
+?- najkrotsza_droga(warszawa, gdansk).
+%Najkrótsza droga z warszawa do gdansk
+ Trasa: [warszawa, gdansk]
+ Koszt: 500 PLN
+ 
+
+ 
